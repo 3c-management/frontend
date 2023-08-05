f6fa65641e8b72b7513e3e25d65002923434bba5
choipaco <kfckim1014@naver.com>
2023-08-05 16:42:06 +0900

    Feat: 간단한 파일정리 및 업로드

---
+++ src/components/HamburgerMenu/HamburgerMenu.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Hamburger: React.FC = () => {
+
+    return (
+      <div>
+        <ul>
+            <li>s</li>
+            <li>s</li>
+            <li>s</li>
+            <li>s</li>
+        </ul>
+      </div>
+    );
+};
+
+export default Hamburger;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Home.module.css'
+import HamburgerMenu from '../../components/HamburgerMenu/HamburgerMenu'
+const Home: React.FC = () => {
+  return (
+   <div>
+    <HamburgerMenu/>
+    asd
+   </div>
+  );
+}
+
+export default Home;
